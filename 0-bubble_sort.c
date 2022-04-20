12bf27cff0710280fa99dc06e8f0f0562f7ceff0
Ahmadj11 <ahmadj.file9@gmail.com>
2022-04-19 20:04:15 -0700

    placeement push

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,1 +1,26 @@
-Bet
+#include "sort.h"
+/*
+ * void bubble_sort - func
+ * Return: 0
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t x, y, temp;
+	x = y = temp = 0;
+	if (size < 2)
+		return (void);
+	for (x = 0; x < size; x++)
+	{
+	for (y = 1; y < (size - x); y++)
+	{
+		if (array[x] < array[x - 1])
+		{
+			temp = array[y - 1];
+			array[y - 1] = array[y];
+			array[y] = temp;
+			print_array(arrat, size);
+		}
+	}
+	}
+}
