6ad97675ac3ca791679b363421048c253ed2deed
Ahmadj11 <ahmadj.file9@gmail.com>
2022-04-19 21:05:38 -0700

    pls end this 2

--- print_array.c
+++ print_array.c
@@ -3,20 +3,20 @@
 
 /**
  * print_array - Prints an array of integers
- * @array: The array 
+ * @array: The array
  * @size: number of elements in array
  */
 void print_array(const int *array, size_t size)
 {
-    size_t x;
+size_t x;
 
-    x = 0;
-    while (array && x < size)
-    {
-        if (x > 0)
-            printf(", ");
-        printf("%d", array[x]);
-        ++x;
-    }
-    printf("\n");
+x = 0;
+while (array && x < size)
+{
+if (x > 0)
+printf(", ");
+printf("%d", array[x]);
+++x;
+}
+printf("\n");
 }
--- print_list.c
+++ print_list.c
@@ -7,16 +7,16 @@
  */
 void print_list(const listint_t *list)
 {
-    int x;
+int x;
 
-    x = 0;
-    while (list)
-    {
-        if (x > 0)
-            printf(", ");
-        printf("%d", list->n);
-        ++x;
-        list = list->next;
-    }
-    printf("\n");
+x = 0;
+while (list)
+{
+if (x > 0)
+printf(", ");
+printf("%d", list->n);
+++x;
+list = list->next;
+}
+printf("\n");
 }
